2a63a97682dac0652b2015206f7502873dd4d067
Thushann1 <21193@students.mrgs.school.nz>
2024-05-15 14:45:06 +1200

    made background

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from tkinter import*
+w_width = 700
+w_height = 500
+
 
 window = Tk()
-window.geometry("834x1194")
+window.geometry(str(w_width) + "x" + str(w_height)) 
 window.title("My App")
+
+main_frame = Frame(background="red", width=100, height=100)
+main_frame.pack()
+
+hello_label = Label(text="hello, World!")
+hello_label.place(x=90, y=20 )
 
 window.mainloop()
