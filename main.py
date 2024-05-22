cad6ec35bb88faf5c21462a07879b0342db9d439
Thushann1 <21193@students.mrgs.school.nz>
2024-05-22 14:50:47 +1200

    made a top and main frame and a home button

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ from tkinter import*
 w_width = 700
 w_height = 500
 
+bg_color = ""
 
 window = Tk()
 window.geometry(str(w_width) + "x" + str(w_height)) 
 window.title("My App")
 
-main_frame = Frame(background="red", width=100, height=100)
+top_frame = Frame(background='red', width=w_width, height=100)
+top_frame.pack()
+
+main_frame = Frame(background="red", width=100, height=1200)
 main_frame.pack()
 
-hello_label = Label(text="hello, World!")
-hello_label.place(x=90, y=20 )
+home_button = Button(bottom_frame, txt="home", height=5, width=5, bg='green'
+home_button.place(x=0,y=0)
+
+# adding widgets to the root window
+
+# Creating a photoimage object to use image
+
+
 
 window.mainloop()
